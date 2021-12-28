fac75d8ad048ae4d7c48727a30743dfae6309c8d
daczarne <daniel.czarnievicz@gmail.com>
2021-12-28 19:03:10 +0100

    core package

---
+++ .github/actions/core_package/index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  
+  /* Logging messages */
+  core.debug('Some debugging message.');
+  core.warning('Some warning message.');
+  core.error('Some error message.');
+  
+  /* Setting secrets */
+  const name = 'Secret to mask in the logs';
+  core.setSecret(name);
+  
+  /* Log the github object */
+  core.startGroup('Logging github object');
+  console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+  
+} catch (error) {
+  
+  core.setFailed(error.message);
+  
+}
